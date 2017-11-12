cf3858d95c345d7b6def38060c74c5aad68dafb4
victoria <vbodnar99@gmail.com>
2017-11-12 20:11:24 +0200

    Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,7 +4,7 @@ belongs_to :user
 has_many :postcomments 
 
 validates :title, :body, presence: true
-validates :text, format: { with: /[a-zA-Z_#][\w.!?,&|@#$%^:;"-]*/ }
+
 validates :title, length:{ in: 1..10}
 
 end
